b782a84c395e27762ffd27fc23c556b9479b3db4
xamcaa <ss2015@ws-2k-114-10n.ccam.cmc.unn.net>
2015-08-24 15:01:03 +0300

    Reworked cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
 
-int main()
+using namespace  cv;
+
+static const char* keys =
 {
-    return 0;
+    "{ | image | | Source image }"
+};
+
+/** @function main */
+int main(int argc, char** argv)
+{
+    cv::CommandLineParser parser( argc, argv, keys );
+    // Parse and validate input parameters
+    std::string image_file = parser.get<std::string>( "image" );
+    Mat src, src_gray;
+    
+    /// Read the image
+    src = imread( image_file );
+
+  if( !src.data )
+    { return -1; }
+
+  /// Convert it to gray
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+
+  /// Reduce the noise so we avoid false circle detection
+  GaussianBlur( src_gray, src_gray, Size(9, 9), 2, 2 );
+
+  vector<Vec3f> circles;
+
+  /// Apply the Hough Transform to find the circles
+  HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/8, 200, 100, 0, 0 );
+
+  /// Draw the circles detected
+  for( size_t i = 0; i < circles.size(); i++ )
+  {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
+   }
+
+  /// Show your results
+  imshow( "Hough Circle Transform Demo", src );
+
+  waitKey(0);
+  return 0;
 }
