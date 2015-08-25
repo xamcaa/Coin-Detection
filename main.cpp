2a7663686b20dd8f0045b0de585dafa2696a6207
xamcaa <ss2015@ws-2k-114-11n.ccam.cmc.unn.net>
2015-08-25 18:10:00 +0300

    threshhold_feature

--- main.cpp
+++ main.cpp
@@ -12,6 +12,14 @@ static const char* keys =
     "{ |  method  |  Hough  |  A method to work with }"
 };
 
+Mat src; Mat src_gray;
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
+int counter = 0;
+
+void thresh_callback(int, void* );
+
 /** @function main */
 int main(int argc, char** argv)
 {
@@ -20,55 +28,84 @@ int main(int argc, char** argv)
     std::string image_file = parser.get<std::string>( "image" );
     std::string method = parser.get<std::string>( "method" );
 
-    Mat src, src_gray;
+//    Mat src, src_gray;
     
     /// Read the image
+    std::cout << image_file << std::endl;
     src = imread( image_file );
-
-  if( !src.data )
-    { return -1; }
-
+    CV_Assert(!src.empty());
+  
   /// Convert it to gray
   cvtColor( src, src_gray, CV_BGR2GRAY );
 
-  /// Increase contrast
-  //equalizeHist(src_gray, src_gray); 
-  //src_gray = src_gray * 2 + 20;
   //imshow( " ", src_gray );
-
-
 
   /// Reduce the noise so we avoid false circle detection
   GaussianBlur( src_gray, src_gray, Size(9, 9), 2, 2);
 
-  vector<Vec3f> circles;
 
-  /// Apply the Hough Transform to find the circles
-  HoughCircles( 
-      src_gray, // image in gray
-      circles, // storage
-      CV_HOUGH_GRADIENT, // method - ?
-      1, // depths
-      src_gray.rows // minimal distance between objects
-      );
+  Mat kernel;
+  kernel = Mat::ones( 6, 6, CV_8UC1);
+  kernel = kernel * 255;
+  morphologyEx(src_gray, src_gray, MORPH_OPEN, kernel);
+  
+  thresh_callback( 0, 0 );
 
-  /// Draw the circles detected
-  for( size_t i = 0; i < circles.size(); i++ )
-  {
-      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-      int radius = cvRound(circles[i][2]);
-      // circle center
-      circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
-      // circle outline
-      circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
-   }
-    
-    std::cout<<"number of coins:\t"<<circles.size()<<std::endl;
-    Canny( src_gray, src_gray, 0, 50 );
+  std::cout<< "number of coins:\t" << counter << std::endl;
+  
   /// Show your results
   imshow( "Hough Circle Transform Demo", src );
   imshow( "Gray scale", src_gray ); 
 
   waitKey(0);
   return 0;
+}
+
+
+void thresh_callback(int, void* )
+{
+  Mat threshold_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  /// Detect edges using Threshold
+  threshold( src_gray, threshold_output, thresh, 255, THRESH_OTSU);
+
+  /// Filling empty spots with Morph 
+
+  imshow("tre", threshold_output);
+
+  /// Find contours
+  findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  /// Find the rotated rectangles and ellipses for each contour
+  vector<RotatedRect> minRect( contours.size() );
+  vector<RotatedRect> minEllipse( contours.size() );
+  int sizeofimage = src.size().height;
+  for( int i = 0; i < contours.size(); i++ )
+     {
+         
+         minRect[i] = minAreaRect( Mat(contours[i]));
+         if ( contours[i].size() > 5 )
+         { 
+             double area = cv::contourArea( contours[i]);
+             if ( ( area < sizeofimage * sizeofimage / 4 ) && ( area > 300 ) )
+             {
+                 minEllipse[i] = fitEllipse( Mat(contours[i]) );
+                 counter++ ;
+             }
+         }
+     }
+
+  /// Draw contours + rotated rects + ellipses
+  Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+  for( int i = 0; i< contours.size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       // contour
+       drawContours( drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+       // ellipse
+       ellipse( drawing, minEllipse[i], color, 2, 8 );
+     }
+  src_gray = drawing;
 }
